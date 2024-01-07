d4ff3cb43f9d002b609d67d52533f8782eb32b3c
Nergiz Nadirli <nergiznadirli@Nergiz-MacBook-Air.local>
2024-01-07 19:52:59 +0400

    proje

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace quiz0701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //5.Verilmiş array - dən verilmiş indexli elementi silən metod.
+            int[] newArr = { 12, 14, 16, 18, 20 };
+            string indexStr=Console.ReadLine();
+            int index = Convert.ToInt32(indexStr);
+            for(int i = 0; i < newArr.Length; i++)
+            {
+                if (newArr[i] == index) 
+                Console.WriteLine(index);
+            }
+        }
+    }
+}
+
